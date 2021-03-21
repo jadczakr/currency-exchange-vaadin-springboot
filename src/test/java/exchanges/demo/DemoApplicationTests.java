01b5e40daa153e48502f71221e0f340c080335fb
Rafal <rafikaski@gmail.com>
2021-03-21 20:50:30 +0100

    First commit. Uploading starting project on github.

---
+++ src/test/java/exchanges/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package exchanges.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
